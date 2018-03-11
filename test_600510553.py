424127c2746f34626d1c1c92a7a88773c747ab23
bossmemory <bossmemorybook@gmail.com>
2018-03-11 14:31:40 +0700

    ......

--- test_600510553.py
+++ test_600510553.py
@@ -1,7 +1,14 @@
 def main():
     a = int(input())
     b = int(input())
-    x = a+b;
+    x = a+b
+    for i in range (x):
+        if i==x:
+            i=x-i
+            print("*._.*")
+            break
+        else:print(i)
+            
     
 if __name__ == "__main__":
     main()
